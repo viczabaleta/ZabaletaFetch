bb5253d6fd72833301c45b88e5f28ad2610464cb
Victoria <mviczabaleta@gmail.com>
2022-08-11 22:57:01 -0300

    Fetch

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
+//fetch
+
+const lista = document.getElementById('listado');
+
+fetch('https://jsonplaceholder.typicode.com/posts')
+    .then( (resp) => resp.json() )
+    .then( (data) => {
+       
+        data.forEach((post) => {
+            const li = document.createElement('li')
+            li.innerHTML = `
+                <h4>${post.title}</h4>
+                <p>${post.body}</p>
+            `
+            lista.append(li)
+        })
+    })
+
+
+
 let personas = [];
 
 let formulario;
